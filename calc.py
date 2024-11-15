4ca4aed0449764ce294631a18c3a296c6aac9380
Alessio Rigamonti <159440780+AleRiga93@users.noreply.github.com>
2024-11-15 13:22:00 +0000

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+num1 = float(input("Write a number: "))
+num2 = float(input("Write the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("The sum is : ", sum)
+print("The difference is : ", difference)
+print("The product is : ", product)
+print("The quotient is : ", round(quotient, 2))
