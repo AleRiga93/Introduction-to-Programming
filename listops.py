3e55addf12ce9cb76e7c0e65675df26f12e0d4e8
Alessio Rigamonti <159440780+AleRiga93@users.noreply.github.com>
2024-11-15 13:24:33 +0000

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,11 @@
+list = [1, 4, 7, 9, 10, 34, 56 , 23, 78, 99]
+
+
+print(list)
+print("The sum of the number in the list is: ",sum(list))
+print("The largest number in the list is", max(list), ", The smallest number is", min(list))
+list.reverse()
+print("The reversed list is :", list)
+print("The largest number in the list is", max(list), ", The smallest number is", min(list))
+
+
