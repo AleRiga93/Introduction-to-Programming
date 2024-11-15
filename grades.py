35183e13f15a26d6229bcca9f5e9c3f52d8eee0d
Alessio Rigamonti <159440780+AleRiga93@users.noreply.github.com>
2024-11-15 13:22:23 +0000

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,15 @@
+student_mark = int(input("Write a mark: "))
+
+if student_mark >= 70 and student_mark <= 100:
+    print("Your grade is A")
+elif student_mark >= 60 and student_mark <=69:
+    print("Your grade is B")
+elif student_mark >= 50 and student_mark <=59:
+    print("Your grade is C")
+elif student_mark >= 40 and student_mark <= 49:
+    print("Your grade is D")
+elif student_mark >= 30 and student_mark <= 39:
+    print("Your grade is E")
+elif student_mark >= 20 and student_mark <= 29:
+    print("Your grade is F")
+
